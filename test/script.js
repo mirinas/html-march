2932a05d29c64621641043e560d8cdafed193a56
mirinas <augustas.mirinas@gmail.com>
2025-04-01 12:11:09 +0300

    added javascript

---
+++ test/script.js
@@ -0,0 +1,26 @@
+
+// alert('Gero antradienio :|')
+
+function toEnglish() {
+    document.querySelector('#title').innerHTML = 'Learning HTML'
+    document.querySelector('#title').style.color = 'red'
+}
+
+
+function toLithuanian() {
+    document.querySelector('#title').innerHTML = 'Mokausi HTML'
+    document.querySelector('#title').style.color = 'green'
+}
+
+
+function checkPassword() {
+    
+    if(document.querySelector('#password').value.length < 5) {
+        document.querySelector('#notification').className = 'error'
+        document.querySelector('#notification').innerHTML = 'Password length must be >5'
+    } else {
+        document.querySelector('#notification').className = 'success'
+        document.querySelector('#notification').innerHTML = 'Strong password'
+    }
+}
+
